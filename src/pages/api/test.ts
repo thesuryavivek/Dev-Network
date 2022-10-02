6fad046d5f2c1138156bcfc73d5317fa594e4b87
surya vivek <suryavivek33377@gmail.com>
2022-10-02 18:11:02 +0530

    testing api endpoints on vercel

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,8 +1,7 @@
-export default async function handler() {
-	fetch("https://jsonplaceholder.typicode.com/todos/1")
-		.then((response) => response.json())
-		.then((json) => {
-			console.log(json);
-			return json;
-		});
+export default async function handler(req: any, res: any) {
+	const response = await fetch(
+		"https://jsonplaceholder.typicode.com/todos/1"
+	);
+	const data = response.json();
+	res.status(200).json(data);
 }
