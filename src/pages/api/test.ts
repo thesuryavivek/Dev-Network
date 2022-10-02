0e5ffc7befbadd988bae4b281e0411a69247d032
surya vivek <suryavivek33377@gmail.com>
2022-10-02 18:14:44 +0530

    testing api

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,7 +1,3 @@
 export default async function handler(req: any, res: any) {
-	const response = await fetch(
-		"https://jsonplaceholder.typicode.com/todos/1"
-	);
-	const data = response.json();
-	res.status(200).json(data);
+	res.status(200).json({ data: "you entered" });
 }
