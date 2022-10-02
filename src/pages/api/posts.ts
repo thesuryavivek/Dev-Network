616c058689f989f258fc3ce1aa783f7ea875c107
surya vivek <suryavivek33377@gmail.com>
2022-10-02 20:15:24 +0530

    commented prisma import in posts endpoint

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,28 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 // import { prisma } from "../../utils/prisma";
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	if (req.method === "POST") {
-		const body = JSON.parse(req.body);
-		await prisma.post.create({
-			data: {
-				title: body.post,
-				authorId: 1,
-			},
-		});
-		console.log(body);
-	}
-	const posts = await prisma.post.findMany({
-		orderBy: {
-			createdAt: "desc",
-		},
-	});
+	// if (req.method === "POST") {
+	// 	const body = JSON.parse(req.body);
+	// 	await prisma.post.create({
+	// 		data: {
+	// 			title: body.post,
+	// 			authorId: 1,
+	// 		},
+	// 	});
+	// 	console.log(body);
+	// }
+	// const posts = await prisma.post.findMany({
+	// 	orderBy: {
+	// 		createdAt: "desc",
+	// 	},
+	// });
 
-	res.status(200).json({ msg: "something", posts });
+	res.status(200).json({ msg: "something" });
 }
