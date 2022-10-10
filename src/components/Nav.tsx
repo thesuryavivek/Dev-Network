60561604d5f7a4340947cfe522e2f7252ba0d5c0
surya vivek <suryavivek33377@gmail.com>
2022-10-10 13:25:41 +0530

    added link tag in logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import Link from "next/link";
 
 const Nav = () => {
 	return (
 		<header className=" z-10 flex w-full items-center justify-between border-b border-blue-300 bg-blue-100 py-4 px-20">
 			<nav>
-				<h1 className="cursor-pointer text-3xl font-bold text-blue-900 text-opacity-80 hover:text-opacity-70">
-					<code>{"< Dev Network />"}</code>
-				</h1>
+				<Link href={"/"}>
+					<a>
+						<h1 className="cursor-pointer text-3xl font-bold text-blue-900 text-opacity-80 hover:text-opacity-70">
+							<code>{"< Dev Network />"}</code>
+						</h1>
+					</a>
+				</Link>
 			</nav>
 			{/* 
 				icons
